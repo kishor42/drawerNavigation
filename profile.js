46b277eff8b869a6947624ab90a3db45fffa57e7
kishor42 <71430973+kishor42@users.noreply.github.com>
2020-10-07 19:48:23 +0545

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View,Text} from 'react-native'
+
+function profile() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>destination screennnnnn</Text>
+      </View>
+    );
+  }
+  export default profile;
